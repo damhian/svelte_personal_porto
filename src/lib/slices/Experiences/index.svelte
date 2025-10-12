d2778d0e4d4cf347ed37a851372bbad79d5e9941
damhian <david.mayshiogie@protonmail.com>
2025-10-12 16:42:22 +0700

    add experiences part

--- src/lib/slices/Experiences/index.svelte
+++ src/lib/slices/Experiences/index.svelte
@@ -1,40 +1,38 @@
 <script lang="ts">
+	import Bounded from '$lib/components/Bounded.svelte';
+	import Heading from '$lib/components/Heading.svelte';
 	import type { Content } from '@prismicio/client';
-	import type { SliceComponentProps } from '@prismicio/svelte';
+	import { PrismicRichText, type SliceComponentProps } from '@prismicio/svelte';
 
 	type Props = SliceComponentProps<Content.ExperiencesSlice>;
 
 	const { slice }: Props = $props();
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use Prismic MCP with your code editor
-	
-	Get AI-powered help to build your slice components — based on your actual model.
-	
-	▶️ Setup:
-	1. Add a new MCP Server in your code editor:
-	
-	{
-	  "mcpServers": {
-	    "Prismic MCP": {
-	      "command": "npx",
-	      "args": ["-y", "@prismicio/mcp-server@latest"]
-	    }
-	  }
-	}
-	
-	2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-	
-	✅ Then open your slice file and ask your code editor:
-		"Code this slice"
-	
-	Your code editor reads your slice model and helps you code faster ⚡
-	🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-	📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
-</section>
+<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<Heading tag="h2" size="lg">
+		{slice.primary.heading}
+	</Heading>
+	{#each slice.primary.items as item}
+		<div class="ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
+			<Heading tag="h3" size="sm">
+				{item.title}
+			</Heading>
+
+			<p
+				class="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-light text-slate-400"
+			>
+				<span>
+					{item.time_period}
+				</span>
+				{' '}
+				<span class="text-7xl font-extralight">/</span>
+				{' '}
+				<span>{item.institution}</span>
+			</p>
+			<div class="prose prose-lg prose-invert mt-4">
+				<PrismicRichText field={item.description} />
+			</div>
+		</div>
+	{/each}
+</Bounded>
